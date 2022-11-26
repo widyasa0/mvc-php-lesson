4ced71e93bc7f725fa90d7fe393c9ad605212a05
widya yasa <widyarusmananda15@gmail.com>
2022-11-26 10:45:08 +0800

    set up routing dan htaccess

--- app/core/App.php
+++ app/core/App.php
@@ -2,8 +2,26 @@
 
 class App
 {
+    //untuk menampilkan nilai url setelah datanya diambil di method parseURL
     public function __construct()
     {
-        echo 'HALO DUNIA';
+        $url = $this->parseURL();
+        var_dump($url);
+    }
+
+    //untuk ngambil data dari url
+    //kalau misalnya ada data di urlnya itu, maka datanya akan diambil menggunakan method parseURL
+    public function parseURL()
+    {
+        if (isset($_GET['url'])){
+
+            //rtrim untuk menghapus slash terakhir di url
+            $url =rtrim( $_GET['url'],'/');
+            //membersihkan url dari karakter aneh
+            $url =filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/',$url);
+            return $url;
+        }
+
     }
 }
