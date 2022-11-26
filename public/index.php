0897ef1addab537a7d6fa71c185607c1c0d9a787
widya yasa <widyarusmananda15@gmail.com>
2022-11-26 09:57:47 +0800

    fist commit

---
+++ public/index.php
@@ -0,0 +1,8 @@
+<?php
+
+//namanya bootstraping, jadi ini manggil satu file disini yaitu init dan file init yang akan memanggil
+//semua file yang akan ditampilkan kepada user
+require_once '../app/init.php';
+
+//class dijalankan
+$app = new App();
