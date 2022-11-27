2ae1d493d9e7f574d3b9a2e274e0b8b5537ac5a9
widya yasa <widyarusmananda15@gmail.com>
2022-11-27 23:59:51 +0800

    add asset

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -1,2 +1,6 @@
+<script src="<?= BASE_URL ?>/assets/vendor/jquery/jquery.min.js"></script>
+<script src="<?= BASE_URL ?>/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+<script src="https://kit.fontawesome.com/9e88c62f38.js" crossorigin="anonymous"></script>
 </body>
 </html>
